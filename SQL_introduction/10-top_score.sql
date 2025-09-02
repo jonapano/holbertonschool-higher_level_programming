ff4f33ddc6efee20c7c7af4fb8c688155552329f
jonapano <panojona1@gmail.com>
2025-09-02 14:32:56 +0200

    removed db name in all files

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script to list all records in second_table ordered by score (descending)
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- script to list records with id = 89 in the table first_table
-SELECT * FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT * FROM first_table WHERE id = 89;

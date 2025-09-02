f163ae4e8526e0025d83d30bf17134cbaa46829f
jonapano <panojona1@gmail.com>
2025-09-02 14:36:37 +0200

    change 8th exercise

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- script to list records with id = 89 in the table first_table
-SELECT * FROM first_table WHERE id = 89;
+-- script to display number of records with id = 89 in the table first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;

45564d8db90aca7705c459ee9f7cc898d6979216
jonapano <panojona1@gmail.com>
2025-09-02 15:25:23 +0200

    exercises 0-3

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- write a script that creates a user with name user_0d_1 and password 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- write a script that grants all privileges to user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

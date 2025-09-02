8e3d0ece98c00b45f9bc874431c3d20ea8107a96
jonapano <panojona1@gmail.com>
2025-09-02 13:21:30 +0200

    exercises 0-9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(id int, name varchar(256), score int);
+
+INSERT INTO hbtn_0c_c.second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);

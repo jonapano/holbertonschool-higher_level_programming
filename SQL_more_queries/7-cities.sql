dd822725cfb4575eaec378bd73e3a0517ad105fb
jonapano <panojona1@gmail.com>
2025-09-02 15:57:45 +0200

    add use database in 7th exercise

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- write a script that creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 -- write a script that creates the table cities
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,

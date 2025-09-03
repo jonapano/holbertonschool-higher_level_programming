ed1eb7170caaf47070ef22bd4f3c587c044782ac
jonapano <panojona1@gmail.com>
2025-09-03 13:02:57 +0200

    changes in the second query

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,4 +7,4 @@ ALTER TABLE hbtn_0c_0.first_table
 
 -- Modify only collation of name column (not character set)
 ALTER TABLE hbtn_0c_0.first_table 
-    MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+    MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

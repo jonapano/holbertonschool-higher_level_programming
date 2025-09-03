6dd82fe523e416d8511128259b498857ae19e3ce
jonapano <panojona1@gmail.com>
2025-09-03 16:08:38 +0200

    changes

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,5 @@
--- import the database dump from temperatures.sql
-USE hbtn_0c_0;
-
-SOURCE /Users/Jona/Downloads/temperatures.sql;
-
 -- write a script that displays the average temperature by city ordered by temperature (descending)
-SELECT city, avg(value) as avg_temp 
+SELECT city, avg(value) AS avg_temp 
 FROM temperatures 
 GROUP BY city 
 ORDER BY avg_temp DESC;
--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,10 +1,5 @@
--- import the database dump from temperatures.sql
-USE hbtn_0c_0;
-
-SOURCE /Users/Jona/Downloads/temperatures.sql;
-
 -- write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT city, AVG(value) as avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
 GROUP BY city

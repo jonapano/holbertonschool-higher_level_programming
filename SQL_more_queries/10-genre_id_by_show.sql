f38d4e32846672b6696f3975f1a64dd186725773
jonapano <panojona1@gmail.com>
2025-09-03 14:08:27 +0200

    add inner to join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,10 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
 USE hbtn_0d_tvshows;
 
 -- write a script that imports the database dump from hbtn_0d_tvshows
-SOURCE /Users/Jona/Downloads/hbtn_0d_tvshows.sql.txt;
+SOURCE /Users/Jona/Downloads/hbtn_0d_tvshows.sql;
 
 -- write a script that lists all shows in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

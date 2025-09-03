3e9d9c28c322eb585e21d042b2dec1a3bdc0d165
jonapano <panojona1@gmail.com>
2025-09-03 13:31:06 +0200

    add comment in exercise 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- write a script that imports the database dump from hbtn_0d_tvshows
 SOURCE /Users/Jona/Downloads/hbtn_0d_tvshows.sql.txt;
 
+-- write a script that lists all shows, and all genres linked to that show
 SELECT tv_shows.title, tv_genres.name 
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id

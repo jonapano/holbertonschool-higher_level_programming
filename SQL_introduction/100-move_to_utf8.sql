73ea60efdfa0053a79bd484694a68165aa317ac0
jonapano <panojona1@gmail.com>
2025-09-03 12:46:44 +0200

    changed varchar to 256

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert table to UTF8
 ALTER TABLE hbtn_0c_0.first_table 
     CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

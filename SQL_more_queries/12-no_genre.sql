6f2edb8887ddf4348b92b72e539c73a473fb15e5
jonapano <panojona1@gmail.com>
2025-09-03 15:19:00 +0200

    changes

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 SOURCE /Users/Jona/Downloads/hbtn_0d_tvshows.sql.txt;
 
 -- write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL

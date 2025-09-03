ab0af2efc80ffd029793bb5e8fc995e33ecddb56
jonapano <panojona1@gmail.com>
2025-09-03 12:59:03 +0200

    changed varchar to 256

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,10 @@
 -- convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- convert table to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    MODIFY name VARCHAR(256) CHARACTER SET DEFAULT COLLATE utf8mb4_unicode_ci;
+
+-- Convert table to utf8mb4 and collation
+ALTER TABLE hbtn_0c_0.first_table 
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Modify only collation of name column (not character set)
+ALTER TABLE hbtn_0c_0.first_table 
+    MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

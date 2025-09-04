cab56958d5f3384fd592e67596440622e3fd0f82
jonapano <panojona1@gmail.com>
2025-09-04 13:02:35 +0200

    3rd advanced exercise

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,8 @@
 -- convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert table to utf8mb4 and collation
-ALTER TABLE hbtn_0c_0.first_table 
-    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- convert table to utf8mb4 and collation
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Modify only collation of name column (not character set)
-ALTER TABLE hbtn_0c_0.first_table 
-    MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- modify only collation of name column (not character set)
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
